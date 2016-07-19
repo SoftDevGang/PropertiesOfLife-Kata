fcc39efd727b0d428bc58668b901ba7d23c3366b
Llewellyn Falco <isidore@setgame.com>
2016-07-19 23:15:01 +0300

    refactored to strings

--- src/org/katas/gameoflife/StillLife.java
+++ src/org/katas/gameoflife/StillLife.java
@@ -16,13 +16,36 @@ public class StillLife implements BoardGenerator {
 
 		Board b = new Board();
 		Random r = new Random();
-		int x = r.nextInt(Integer.MAX_VALUE - 1);
-		int y = r.nextInt(Integer.MAX_VALUE - 1);
-		b.place(new Point(x + 0, y + 0));
-		b.place(new Point(x + 1, y + 0));
-		b.place(new Point(x + 0, y + 1));
-		b.place(new Point(x + 1, y + 1));
+		int x = r.nextInt(Integer.MAX_VALUE - 3);
+		int y = r.nextInt(Integer.MAX_VALUE - 3);
+
+		place(b, x, y, getRandomStillLife(r));
+		// createBeehive(b, x, y);
 		return b;
+	}
+
+	private String getRandomStillLife(Random r) {
+		String block = "**\n**\n";
+		String beehive = " ** \n*  *\n ** ";
+		String[] stillLifes = { block, beehive };
+		String stillLife = stillLifes[r.nextInt(stillLifes.length)];
+		return stillLife;
+	}
+
+	private void place(Board b, int x, int y, String string) {
+
+		int originalX = x;
+		for (Character c : string.toCharArray()) {
+			if (c == '*') {
+				b.place(new Point(x, y));
+				x++;
+			} else if (c == '\n') {
+				y++;
+				x = originalX;
+			} else {
+				x++;
+			}
+		}
 	}
 
 }
